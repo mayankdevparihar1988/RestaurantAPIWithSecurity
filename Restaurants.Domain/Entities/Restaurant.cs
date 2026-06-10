a3e46ca1aad75e9f5af68602f77b15c87452dae1
mayankparihar <mayank.dev.parihar1988gmail.com>
2026-06-10 11:00:21 +0200

    Some other files

---
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurants.Domain.Entities;
+
+public class Restaurant
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public bool HasDelivery { get; set; }
+
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+
+    public Address? Address { get; set; }
+    public List<Dish> Dishes { get; set; } = new();
+
+    // public User Owner { get; set; } = default!;
+   // public string OwnerId { get; set; } = null!;
+  // public string? LogoSasUrl { get; set; }
+}
